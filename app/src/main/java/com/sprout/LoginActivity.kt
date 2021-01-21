b14d20fa75d47435c49163af15745e59d3cb5dc0
2575756067@qq.com <2575756067@qq.com>
2021-01-21 09:45:58 +0800

    登录页背景视频

--- app/src/main/java/com/sprout/LoginActivity.kt
+++ app/src/main/java/com/sprout/LoginActivity.kt
@@ -2,8 +2,6 @@ package com.sprout
 
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
