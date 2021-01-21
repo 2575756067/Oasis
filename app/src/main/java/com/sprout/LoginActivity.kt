aa470848829b6223e4b21d84207541e3593d8ac8
2575756067@qq.com <2575756067@qq.com>
2021-01-21 09:44:33 +0800

    登录页背景视频

--- app/src/main/java/com/sprout/LoginActivity.kt
+++ app/src/main/java/com/sprout/LoginActivity.kt
@@ -1,4 +1,24 @@
 package com.sprout
 
-class LoginActivity {
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        initView()
+    }
+
+    private fun initView() {
+        //todo 加载视频资源控件
+        videoview!!.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.loginbackvideo));
+        videoview.start() //todo 开始播放
+        videoview.setOnCompletionListener { videoview.start() } //todo 循环播放
+    }
 }
